4ca0b8be2f03c626eb1c3f1aff30d7404ec4ae32
Likhit Garimella <likhitgmella@gmail.com>
2023-07-28 02:15:18 +0530

    backend init with imports & cors

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,45 @@
-from fastapi import FastAPI
+# uvicorn main:app
+# uvicorn main:app --reload
 
+# Main Imports
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config #allows to bring in variables from .env setup section
+import openai
+
+
+# Custom Function Imports
+# ...
+
+
+# Initiate App
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+# CORS: Cross-Origin Resource Sharing
+# CORS - Origins
+origins = [
+    "http://localhost:5173/",
+    "http://localhost:5174/",
+    "http://localhost:4173/",
+    "http://localhost:4174/",
+    "http://localhost:3000/"
+]
+
+
+# CORS - Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Check Health
+@app.get("/health")
+async def check_health():
     print("hello")
-    return {"message": "Hello World"}
+    return {"message": "Hello Likhit"}

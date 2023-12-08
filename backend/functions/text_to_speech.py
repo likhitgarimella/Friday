f7ef29ff74c370513124edb1d94caab1cf4aa488
Likhit Garimella <likhitgmella@gmail.com>
2023-12-08 09:05:17 -0500

    part #45 done

--- backend/functions/text_to_speech.py
+++ backend/functions/text_to_speech.py
@@ -2,3 +2,36 @@ import requests
 from decouple import config
 
 ELEVEN_LABS_API_KEY = config("ELEVEN_LABS_API_KEY")
+
+# Eleven Labs
+# Convert Text to Speech
+def convert_text_to_speech(message):
+
+    # Define data (body)
+    body = {
+        "text": message,
+        "voice settings": {
+            "stability": 0,
+            "similarity_boost": 0,
+        }
+    }
+
+    # Define voice
+    voice_friday = "21m00Tcm4TlvDq8ikWAM"
+
+    # Constructing headers & endpoint
+    headers = {"xi-api-key": ELEVEN_LABS_API_KEY, "Content-Type": "application/json", "accept": "audio/mpeg"}
+    endpoint = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_friday}"
+
+    # Send request
+    try:
+        response = requests.post(endpoint, json=body, headers=headers)
+    except Exception as e:
+        return
+    
+    # Handle Response
+    if response.status_code == 200:
+        # returns audio file we want
+        return response.content
+    else:
+        return
--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ import openai
 # Custom Function Imports
 from functions.database import store_messages, reset_messages
 from functions.openai_requests import convert_audio_to_text, get_chat_response
+from functions.text_to_speech import convert_text_to_speech
 
 # Initiate App
 app = FastAPI()
@@ -65,11 +66,29 @@ async def get_audio():
     # Get ChatGPT Response
     chat_response = get_chat_response(message_decoded)
 
+    # Guard: Ensure chat response
+    if not chat_response:
+        return HTTPException(status_code=400, detail="Failed to get chat response")
+
     # Store messages
     store_messages(message_decoded, chat_response)
     # message_decoded is our msg we send to chatgpt, chat_response is chatgpt's response
 
     print(chat_response)
+
+    # Convert chat response to audio
+    audio_output = convert_text_to_speech(chat_response)
+
+    # Guard: Ensure audio response
+    if not audio_output:
+        return HTTPException(status_code=400, detail="Failed to get Elevent Labs audio response")
+    
+    # Create a generator that yields chunks of data
+    def iterfile():
+        yield audio_output
+    
+    # Return audio file
+    return StreamingResponse(iterfile(), media_type="audio/mpeg")
 
     return "Done"
 

02676db768b66d78f239b80a3e07417673710506
Likhit Garimella <likhitgmella@gmail.com>
2023-12-09 05:18:44 -0500

    title tsx setup

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -1,8 +1,8 @@
-import {useState} from "react"
+import {useState} from "react";
 
 function Controller() {
 
-    const [isLoading, setIsLoading] = useState[false];
+    const [isLoading, setIsLoading] = useState(false);
     const [messages, setMessages] = useState<any[]>([]);
 
     const createBlobUrl = (data: any) => {};

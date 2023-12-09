b70b51c5fa865fddd41594418e20af82d401241d
Likhit Garimella <likhitgmella@gmail.com>
2023-12-09 05:12:00 -0500

    structuring controller component

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -1,7 +1,22 @@
-import React from "react"
+import {useState} from "react"
 
 function Controller() {
-  return <div>Controller</div>
+
+    const [isLoading, setIsLoading] = useState[false];
+    const [messages, setMessages] = useState<any[]>([]);
+
+    const createBlobUrl = (data: any) => {};
+
+    const handleStop = async () => {};
+    
+    return (
+        <div className="h-screen overflow-y-hidden">
+            <div>Title</div>
+            <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
+                Placeholder
+            </div>
+        </div>
+    );
 }
 
 export default Controller;

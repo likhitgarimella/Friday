cbdffdfbee6591a2308ddd4e871a549ba425973c
Likhit Garimella <likhitgmella@gmail.com>
2023-12-10 17:42:32 -0500

    connecting frontend to backend #1; #54 done

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -15,6 +15,24 @@ function Controller() {
 
     const handleStop = async (blobUrl: string) => {
         console.log(blobUrl)
+        setIsLoading(true)
+
+        // Append recorded message to messages
+        const myMessage = { sender: "Me", blobUrl };
+        const messagesArr = [...messages, myMessage];
+
+        // Convert blob url to blob object => to send this to backend
+        fetch(blobUrl)
+            .then((res) => res.blob())
+            .then(async (blob) => {
+
+                // Construct audio to send file
+                const formData = new FormData();
+                formData.append("file", blob, "myFile.wav");
+
+            });
+
+        setIsLoading(false)
     };
     
     return (
